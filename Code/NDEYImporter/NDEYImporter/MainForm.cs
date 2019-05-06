482c9866c423d86b3beb6191df3c614e7bdf3459
wcss <wcss2020@gmail.com>
2019-05-06 15:43:05 +0800

    ......

--- Code/NDEYImporter/NDEYImporter/MainForm.cs
+++ Code/NDEYImporter/NDEYImporter/MainForm.cs
@@ -18,9 +18,9 @@ namespace NDEYImporter
 
         private void btnTest_Click(object sender, EventArgs e)
         {
-            NDEYImporter.Util.DBImporter.appendNdeyToLocalDB(@"C:\Users\wcss\Desktop\AA\myData.db");
+            NDEYImporter.Util.DBImporter.appendNdeyToLocal(@"C:\Users\wcss\Desktop\AA\myData.db");
 
-            NDEYImporter.Util.DBImporter.appendNdeyToLocalDB(@"C:\Users\wcss\Desktop\BB\myData.db");
+            NDEYImporter.Util.DBImporter.appendNdeyToLocal(@"C:\Users\wcss\Desktop\BB\myData.db");
         }
     }
 }
