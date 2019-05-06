54592ddb50ba9a7d670f9d80d475ae57436b8a49
wcss <wcss2020@gmail.com>
2019-05-06 10:55:23 +0800

    ...

--- Code/NDEYImporter/NDEYImporter/Program.cs
+++ Code/NDEYImporter/NDEYImporter/Program.cs
@@ -13,6 +13,9 @@ namespace NDEYImporter
         [STAThread]
         static void Main()
         {
+            //初始化本地数据库
+            NDEYImporter.DB.ConnectionManager.Open(System.IO.Path.Combine(Application.StartupPath, "static.db"));
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
