6a842fdf5f7998b32291562137e37216da57daf9
wcss <wcss2020@gmail.com>
2019-05-06 17:06:30 +0800

    ...

--- Code/NDEYImporter/NDEYImporter/MainForm.cs
+++ Code/NDEYImporter/NDEYImporter/MainForm.cs
@@ -11,14 +11,38 @@ namespace NDEYImporter
 {
     public partial class MainForm : Form
     {
+        /// <summary>
+        /// NDEY上报包目录
+        /// </summary>
+        public static string PackageDir = System.IO.Path.Combine(Application.StartupPath, "PackageDir");
+
+        /// <summary>
+        /// NDEY上报包解压目录
+        /// </summary>
+        public static string DBTempDir = System.IO.Path.Combine(Application.StartupPath, "DBTempDir");
+
         public MainForm()
         {
             InitializeComponent();
+
+            //创建NDEY上报包目录
+            try
+            {
+                System.IO.Directory.CreateDirectory(PackageDir);
+            }
+            catch (Exception ex) { }
+
+            //创建NDEY上报包解压目录
+            try
+            {
+                System.IO.Directory.CreateDirectory(DBTempDir);
+            }
+            catch (Exception ex) { }
         }
 
         private void btnTest_Click(object sender, EventArgs e)
         {
-            NDEYImporter.Util.DBImporter.appendNdeyToLocal(@"C:\Users\wcss\Desktop\Current\myData.db");
+            NDEYImporter.Util.DBImporter.appendNdeyToLocal(@"C:\Users\wcss\Desktop\BB\myData.db");
 
             //NDEYImporter.Util.DBImporter.appendNdeyToLocal(@"C:\Users\wcss\Desktop\BB\myData.db");
         }
