00817881e9c1730d527949efcdad971919644329
wcss <wcss2020@gmail.com>
2019-05-09 16:50:56 +0800

    ...

--- Code/NDEYImporter/NDEYImporter/Forms/CheckNeedReplaceForm.Designer.cs
+++ Code/NDEYImporter/NDEYImporter/Forms/CheckNeedReplaceForm.Designer.cs
@@ -28,7 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CheckNeedReplaceForm));
             this.tvSubDirs = new System.Windows.Forms.TreeView();
+            this.ilDirIcon = new System.Windows.Forms.ImageList(this.components);
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.lvErrorList = new System.Windows.Forms.ListView();
             this.chID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -49,11 +52,20 @@
             this.tvSubDirs.CheckBoxes = true;
             this.tvSubDirs.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tvSubDirs.FullRowSelect = true;
+            this.tvSubDirs.ImageIndex = 0;
+            this.tvSubDirs.ImageList = this.ilDirIcon;
             this.tvSubDirs.Location = new System.Drawing.Point(0, 0);
             this.tvSubDirs.Name = "tvSubDirs";
-            this.tvSubDirs.Size = new System.Drawing.Size(317, 503);
+            this.tvSubDirs.SelectedImageIndex = 0;
+            this.tvSubDirs.Size = new System.Drawing.Size(420, 646);
             this.tvSubDirs.TabIndex = 0;
             this.tvSubDirs.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.tvSubDirs_AfterCheck);
+            // 
+            // ilDirIcon
+            // 
+            this.ilDirIcon.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ilDirIcon.ImageStream")));
+            this.ilDirIcon.TransparentColor = System.Drawing.Color.Transparent;
+            this.ilDirIcon.Images.SetKeyName(0, "a9.png");
             // 
             // splitContainer1
             // 
@@ -69,8 +81,8 @@
             // 
             this.splitContainer1.Panel2.Controls.Add(this.lvErrorList);
             this.splitContainer1.Panel2.Controls.Add(this.groupBox2);
-            this.splitContainer1.Size = new System.Drawing.Size(952, 503);
-            this.splitContainer1.SplitterDistance = 317;
+            this.splitContainer1.Size = new System.Drawing.Size(1264, 646);
+            this.splitContainer1.SplitterDistance = 420;
             this.splitContainer1.TabIndex = 1;
             // 
             // lvErrorList
@@ -81,7 +93,7 @@
             this.lvErrorList.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lvErrorList.Location = new System.Drawing.Point(0, 43);
             this.lvErrorList.Name = "lvErrorList";
-            this.lvErrorList.Size = new System.Drawing.Size(631, 460);
+            this.lvErrorList.Size = new System.Drawing.Size(840, 603);
             this.lvErrorList.TabIndex = 0;
             this.lvErrorList.UseCompatibleStateImageBehavior = false;
             this.lvErrorList.View = System.Windows.Forms.View.Details;
@@ -98,7 +110,7 @@
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
             this.groupBox2.Location = new System.Drawing.Point(0, 0);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(631, 43);
+            this.groupBox2.Size = new System.Drawing.Size(840, 43);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             // 
@@ -108,7 +120,7 @@
             this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label1.Location = new System.Drawing.Point(3, 17);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(625, 23);
+            this.label1.Size = new System.Drawing.Size(834, 23);
             this.label1.TabIndex = 0;
             this.label1.Text = "确定需要替换下面的项目？";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -118,16 +130,16 @@
             this.groupBox1.Controls.Add(this.btnOK);
             this.groupBox1.Controls.Add(this.btnCancel);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.groupBox1.Location = new System.Drawing.Point(0, 503);
+            this.groupBox1.Location = new System.Drawing.Point(0, 646);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(952, 61);
+            this.groupBox1.Size = new System.Drawing.Size(1264, 61);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             // 
             // btnOK
             // 
             this.btnOK.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btnOK.Location = new System.Drawing.Point(799, 17);
+            this.btnOK.Location = new System.Drawing.Point(1111, 17);
             this.btnOK.Name = "btnOK";
             this.btnOK.Size = new System.Drawing.Size(75, 41);
             this.btnOK.TabIndex = 1;
@@ -138,7 +150,7 @@
             // btnCancel
             // 
             this.btnCancel.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btnCancel.Location = new System.Drawing.Point(874, 17);
+            this.btnCancel.Location = new System.Drawing.Point(1186, 17);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(75, 41);
             this.btnCancel.TabIndex = 0;
@@ -150,11 +162,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(952, 564);
+            this.ClientSize = new System.Drawing.Size(1264, 707);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "CheckNeedReplaceForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -179,5 +190,6 @@
         private System.Windows.Forms.ColumnHeader chID;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ImageList ilDirIcon;
     }
 }
