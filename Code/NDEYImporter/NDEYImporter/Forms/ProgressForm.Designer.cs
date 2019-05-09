b362f7f1de576a3192b0d1a9c928f40f665824db
wcss <wcss2020@gmail.com>
2019-05-09 10:55:50 +0800

    ...

---
+++ Code/NDEYImporter/NDEYImporter/Forms/ProgressForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace NDEYImporter.Forms
+{
+    partial class ProgressForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbProgress = new System.Windows.Forms.ProgressBar();
+            this.lblProgressText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pbProgress
+            // 
+            this.pbProgress.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pbProgress.Location = new System.Drawing.Point(0, 0);
+            this.pbProgress.Name = "pbProgress";
+            this.pbProgress.Size = new System.Drawing.Size(412, 23);
+            this.pbProgress.TabIndex = 0;
+            // 
+            // lblProgressText
+            // 
+            this.lblProgressText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblProgressText.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblProgressText.Location = new System.Drawing.Point(0, 23);
+            this.lblProgressText.Name = "lblProgressText";
+            this.lblProgressText.Size = new System.Drawing.Size(412, 23);
+            this.lblProgressText.TabIndex = 1;
+            this.lblProgressText.Text = "...";
+            this.lblProgressText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ProgressForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 46);
+            this.Controls.Add(this.lblProgressText);
+            this.Controls.Add(this.pbProgress);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ProgressForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ProgressForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar pbProgress;
+        private System.Windows.Forms.Label lblProgressText;
+    }
+}
---
+++ Code/NDEYImporter/NDEYImporter/Forms/ProgressForm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NDEYImporter.Forms
+{
+    /// <summary>
+    /// 进度条窗体
+    /// </summary>
+    public partial class ProgressForm : Form
+    {
+        /// <summary>
+        /// 工作线程
+        /// </summary>
+        private BackgroundWorker worker = new BackgroundWorker();
+
+        /// <summary>
+        /// 工作事件
+        /// </summary>
+        private EventHandler ehDynamicMethod = null;
+
+        public ProgressForm()
+        {
+            InitializeComponent();
+
+            worker.WorkerSupportsCancellation = true;
+            worker.DoWork += worker_DoWork;
+        }
+
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //运行工作事件
+            if (ehDynamicMethod != null)
+            {
+                ehDynamicMethod(this, new EventArgs());
+            }
+        }
+
+        /// <summary>
+        /// 运行代码
+        /// </summary>
+        /// <param name="total">总进度</param>
+        /// <param name="cur">当前进度</param>
+        /// <param name="ehDynamic">工作事件</param>
+        public void run(int total, int cur, EventHandler ehDynamic)
+        {
+            //设置进度
+            pbProgress.Maximum = total;
+            pbProgress.Value = cur;
+
+            //设置工作事件
+            ehDynamicMethod = ehDynamic;
+
+            //检查工作线程是否正在运行
+            if (worker.IsBusy)
+            {
+                return;
+            }
+            else
+            {
+                //运行工作线程
+                worker.RunWorkerAsync();
+            }
+        }
+
+        /// <summary>
+        /// 设置当前进度
+        /// </summary>
+        /// <param name="current">进度值</param>
+        public void reportProgress(int current,string text)
+        {
+            if (IsHandleCreated)
+            {
+                Invoke(new MethodInvoker(delegate()
+                    {
+                        //设置进度
+                        pbProgress.Value = current;
+
+                        //设置文本
+                        lblProgressText.Text = text;
+                    }));
+            }
+        }
+    }
+}
