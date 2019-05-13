71bc88643ebf0761b75ad2b00de8f4068354d0ba
wcss <wcss2020@gmail.com>
2019-05-13 15:01:30 +0800

    。。。。。。

--- Code/NDEYImporter/NDEYImporter/Forms/UnZipDocsForm.Designer.cs
+++ Code/NDEYImporter/NDEYImporter/Forms/UnZipDocsForm.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.tvSubDirs = new System.Windows.Forms.TreeView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tvSubDirs
@@ -38,20 +42,54 @@
             this.tvSubDirs.FullRowSelect = true;
             this.tvSubDirs.Location = new System.Drawing.Point(0, 0);
             this.tvSubDirs.Name = "tvSubDirs";
-            this.tvSubDirs.Size = new System.Drawing.Size(292, 494);
+            this.tvSubDirs.Size = new System.Drawing.Size(545, 503);
             this.tvSubDirs.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnOK);
+            this.panel1.Controls.Add(this.btnCancel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 503);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(545, 41);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnCancel.Location = new System.Drawing.Point(461, 0);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(84, 41);
+            this.btnCancel.TabIndex = 0;
+            this.btnCancel.Text = "取消";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnOK.Location = new System.Drawing.Point(377, 0);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(84, 41);
+            this.btnOK.TabIndex = 1;
+            this.btnOK.Text = "确定";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
             // 
             // UnZipDocsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 494);
+            this.ClientSize = new System.Drawing.Size(545, 544);
             this.Controls.Add(this.tvSubDirs);
+            this.Controls.Add(this.panel1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "UnZipDocsForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "请选择要解压的项目!";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -59,5 +97,8 @@
         #endregion
 
         private System.Windows.Forms.TreeView tvSubDirs;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
     }
 }
--- Code/NDEYImporter/NDEYImporter/Forms/UnZipDocsForm.cs
+++ Code/NDEYImporter/NDEYImporter/Forms/UnZipDocsForm.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using NDEYImporter.Util;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Text;
 using System.Windows.Forms;
 
@@ -53,6 +55,119 @@ namespace NDEYImporter.Forms
                 foreach (TreeNode tn in tvSubDirs.Nodes)
                 {
                     tn.Checked = true;
+                }
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = System.Windows.Forms.DialogResult.Cancel;
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            //需要替换的申报包列表
+            List<string> importList = new List<string>();
+
+            #region 查的需要导入的路径
+            foreach (TreeNode tn in tvSubDirs.Nodes)
+            {
+                //判断是否被选中
+                if (tn.Checked)
+                {
+                    //读取目录名称中的项目编号
+                    string projectNumber = tn.Text.Substring(0, 11);
+
+                    //将需要处理的申报包添加到列表中
+                    importList.Add(Path.Combine(MainForm.Config.TotalDir, tn.Text));
+
+                }
+            }
+            #endregion
+
+            //开始导入
+            ProgressForm pf = new ProgressForm();
+            pf.Show();
+            pf.run(importList.Count, 0, new EventHandler(delegate(object senders, EventArgs ee)
+            {
+                //进度数值
+                int progressVal = 0;
+                //导入
+                foreach (string pDir in importList)
+                {
+                    progressVal++;
+
+                    //读取项目ID
+                    string projectNumber = new DirectoryInfo(pDir).Name.Substring(0, 11);
+
+                    //报告进度
+                    pf.reportProgress(progressVal, string.Empty);
+
+                    //报告进度
+                    pf.reportProgress(progressVal, projectNumber + "_开始解压");
+
+                    //解压ZIP包
+                    unZipDocFiles(projectNumber);
+
+                    //报告进度
+                    pf.reportProgress(progressVal, projectNumber + "_结束解压");
+                }
+
+                //检查是否已创建句柄，并调用委托执行UI方法
+                if (pf.IsHandleCreated)
+                {
+                    pf.Invoke(new MethodInvoker(delegate()
+                    {
+                        //刷新Catalog列表
+                        MainForm.Instance.reloadCatalogList();
+
+                        //关闭进度窗口
+                        pf.Close();
+                    }));
+                }
+            }));
+
+            //关闭窗口
+            Close();
+        }
+
+        /// <summary>
+        /// 从申报包中解压文档
+        /// </summary>
+        /// <param name="projectNumber">项目编号</param>
+        private void unZipDocFiles(string projectNumber)
+        {
+            //查找指定申报包
+            string[] subDirs = Directory.GetDirectories(MainForm.Config.TotalDir);
+            if (subDirs != null)
+            {
+                //循环子目录,查找与项目编号相符的目录名称
+                foreach (string s in subDirs)
+                {
+                    //获得目录信息
+                    DirectoryInfo di = new DirectoryInfo(s);
+
+                    //判断目录是否符合条件
+                    if (di.Name.Contains(projectNumber) && di.Name.Length >= 12)
+                    {
+                        //查找子文件
+                        string[] subFiles = Directory.GetFiles(s);
+                        if (subFiles != null && subFiles.Length >= 1)
+                        {
+                            //解压这个Zip包
+                            string destDir = Path.Combine(MainForm.Config.UnZipDir, projectNumber);
+
+                            //创建临时目录
+                            try
+                            {
+                                Directory.CreateDirectory(destDir);
+                            }
+                            catch (Exception ex) { }
+
+                            //解压这个包
+                            new NdeyDocFilesUnZip().UnZipFile(subFiles[0], destDir, string.Empty, true);
+                        }
+                    }
                 }
             }
         }
